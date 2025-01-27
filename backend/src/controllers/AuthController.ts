42a6d0946740beab4a33256b0444ef8b2a555884
Joaquin Cardoso <joaquin.cardoso.r@gmail.com>
2025-01-28 00:41:26 +0800

    back-feat/validate user already exist

--- backend/src/controllers/AuthController.ts
+++ backend/src/controllers/AuthController.ts
@@ -1,7 +1,34 @@
 import type {Request, Response} from 'express'
 
+import { handleError } from '../utils'
+import User from '../models/User'
+import { hashPassword } from '../utils/auth'
+
 export class AuthController {
     static createAccount = async (req : Request ,res: Response)  => {
-        res.send('Hello World from Controller')
+        try {
+            const { password, email } = req.body
+
+            // Prevent duplicated
+            const userExist = await User.findOne({email})
+            if(userExist){
+                const error = new Error('User already exist')
+                res.status(409).json([error.message])
+            }
+            
+            // Create a user
+            const user = new User(req.body)
+
+            // Hash password
+            user.password = await hashPassword(password)
+            
+            //Save new user
+            await user.save()
+            res.send('Account succesfully created, confirm your email to login')
+
+        } catch (error) {
+            handleError(res, error, "Failed to create the task")
+        }
+        
     }
 }
