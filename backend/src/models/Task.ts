48e5378f4bb053cba5cadaacf8d46f5ae8a11c1d
Joaquin Cardoso <joaquin.cardoso.r@gmail.com>
2025-01-27 23:13:38 +0800

    back-fix/typo in Task model

--- backend/src/models/Task.ts
+++ backend/src/models/Task.ts
@@ -5,7 +5,7 @@ const taskStatus = {
     ON_HOLD: 'onHold',
     IN_PROGRESS : 'inProgress',
     UNDER_REVIEW: 'underReview',
-    COMPLETED: 'completes'
+    COMPLETED: 'completed'
 } as const
 
 export type TaskStatus = typeof taskStatus[keyof typeof taskStatus]
