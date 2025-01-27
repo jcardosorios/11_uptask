7c204a01da3d07951551be956e7c97291de7f5b7
Joaquin Cardoso <joaquin.cardoso.r@gmail.com>
2025-01-28 00:06:40 +0800

    back-feat/validation body for create User

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,9 +1,27 @@
 import { Router } from 'express'
+import { body } from 'express-validator'
+import { AuthController } from '../controllers/AuthController'
+import { handleInputErrors } from '../middleware/validation'
 
 const router = Router()
 
-router.get('/', (req,res) => {
-    res.send('Hello World')
-})
+// Create Account
+router.post('/create-account',
+    // Validation
+    body('name')
+        .notEmpty().withMessage('Name is required'),
+    body('password')
+        .isLength({ min: 8}).withMessage('Password must be at least 8 characters long'),
+    body('password-confirmation').custom((value, { req }) => {
+        if(value !== req.body.password){
+            throw new Error('Passwords do not match')
+        }
+        return true
+    }),
+    body('email')
+        .isEmail().withMessage('Email must be valid'),
+    handleInputErrors,
+    AuthController.createAccount
+)
 
 export default router
