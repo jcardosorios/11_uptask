79ec96e0f94954bca44887a4ffbc9645b2240156
Joaquin Cardoso <joaquin.cardoso.r@gmail.com>
2025-02-08 20:10:38 -0300

    front-feat/create view to update user profile

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -26,6 +26,8 @@ export const userSchema = authSchema.pick({
 })
 
 export type User = z.infer<typeof userSchema>
+export type UserProfileForm = Pick<User, 'name' | 'email'>
+
 
 /** Notes  */
 const noteSchema = z.object({
--- frontend/src/views/profile/ProfileView.tsx
+++ frontend/src/views/profile/ProfileView.tsx
@@ -1,7 +1,15 @@
+import ProfileForm from "@/components/profile/ProfileForm"
+import { useAuth } from "@/hooks/useAuth"
 
 
 export default function ProfileView() {
-  return (
-    <div>ProfileView</div>
-  )
+    const { data, isLoading } = useAuth()
+
+    if(isLoading) return (<div>Loading...</div>)
+    if (data) return (
+        <>
+            <div>ProfileView</div>
+            <ProfileForm data={data} />
+        </>
+    )
 }
