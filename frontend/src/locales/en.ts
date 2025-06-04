fc8685d712a3aaac2bba8bd8b2c9d8624823d047
Joaquin Cardoso Rios <105686879+jcardosorios@users.noreply.github.com>
2025-06-04 18:17:28 -0400

    Delete frontend directory

--- frontend/src/locales/en.ts
+++
@@ -1,7 +0,0 @@
-export const statusTranslations : {[key : string] : string} = {
-    pending: 'Pending',
-    onHold: 'On Hold',
-    inProgress: 'In Progress',
-    underReview: 'Under Review',
-    completed: 'Completed',
-}
--- frontend/src/utils/policies.ts
+++
@@ -1,5 +0,0 @@
-import { Project, User } from "../types"
-
-export const isManager = (managerId: Project['manager'], userId: User['_id']) => {
-    return managerId === userId
-}
