d952495064ecf2d2e6025c573c40cadb47cdbee2
Joaquin Cardoso Rios <105686879+jcardosorios@users.noreply.github.com>
2025-06-04 18:17:06 -0400

    Delete backend directory

--- backend/src/index.ts
+++
@@ -1,9 +0,0 @@
-import colors from 'colors'
-import server from './server'
-
-
-const port = process.env.PORT || 4000
-
-server.listen(port, () => {
-    console.log(colors.cyan.bold(`Server listening at port ${port}`))
-})
